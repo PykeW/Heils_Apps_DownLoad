e51f0392d1246cf6568ffceafa715a979072dd95
jie Lu <lujie@heils.cn>
2024-11-28 16:53:38 +0800

    钉钉扫码环境初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,12 +7,17 @@ import "./mock";
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
+  <React.StrictMode>  
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
+  </React.StrictMode>
 );
+
+// 添加这行来调用 reportWebVitals
+reportWebVitals();
