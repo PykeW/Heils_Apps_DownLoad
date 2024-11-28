731c5faad48c6d4b3c80cf1ee364e20dfa19ba86
jie Lu <lujie@heils.cn>
2024-11-28 15:48:07 +0800

    对一些依赖的更新

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+import type { Metric } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then((webVitals) => {
+      webVitals.onCLS(onPerfEntry);
+      webVitals.onFID(onPerfEntry);
+      webVitals.onFCP(onPerfEntry);
+      webVitals.onLCP(onPerfEntry);
+      webVitals.onTTFB(onPerfEntry);
+    }).catch((error) => {
+      console.error('Failed to load web-vitals', error);
     });
   }
 };
